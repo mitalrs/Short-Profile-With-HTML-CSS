11da0d4c3947f5b0af3b2cff3867680b7ec57ea1
bhaitabahi <bhaitabahi@localhost.localdomain>
2022-10-06 11:30:54 -0400

    By bhaitabahi

---
+++ Bhaitabahi/js/main.js
@@ -0,0 +1,43 @@
+(function ($) {
+    "use strict";
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+    
+    
+    // Skills
+    $('.skills').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+})(jQuery);
+
--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -3,3 +3,4 @@
 - [Mital](https://github.com/mitalrs)
 - [Harsh](https://github.com/Harsh-Singh-Rajput)
 - [Raunak](https://github.com/raunak-dev-edu)
+- [Bhai Tabahi](https://github.com/bhaitabahi786)
